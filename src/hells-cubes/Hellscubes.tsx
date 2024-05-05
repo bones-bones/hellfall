f535213ebed589f527d5038b34df00bff0c953bf
bones-bones <elliot.ma.brown@gmail.com>
2024-05-05 13:16:44 -0700

    quick dl

--- src/hells-cubes/Hellscubes.tsx
+++ src/hells-cubes/Hellscubes.tsx
@@ -3,8 +3,11 @@ import { HellsCube } from "./one";
 import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 import { Header } from "../header";
+import { toDeck } from "../deck-builder/toDeck";
+import { useCards } from "../hellfall/useCards";
 
 export const Hellscubes = () => {
+  const cards = useCards();
   return (
     <>
       <Header></Header>
@@ -21,6 +24,32 @@ export const Hellscubes = () => {
               <h3>
                 <StyledLink to="one">HC1</StyledLink>
               </h3>
+              <button
+                onClick={() => {
+                  const filtered = cards.filter((e) => e.Set === "HC3");
+                  const toSaveNumbers: number[] = [];
+                  while (toSaveNumbers.length < 90) {
+                    const toPick = Math.floor(Math.random() * filtered.length);
+                    if (!toSaveNumbers.includes(toPick)) {
+                      toSaveNumbers.push(toPick);
+                    }
+                  }
+                  console.log(toSaveNumbers);
+                  const val = toDeck(toSaveNumbers.map((e) => filtered[e]));
+                  const url =
+                    "data:text/plain;base64," +
+                    btoa(unescape(encodeURIComponent(JSON.stringify(val))));
+                  const a = document.createElement("a");
+                  a.style.display = "none";
+                  a.href = url;
+                  // the filename you want
+                  a.download = "HC3 90 cards" + ".json";
+                  document.body.appendChild(a);
+                  a.click();
+                }}
+              >
+                download 90 cards for winston HC3
+              </button>
             </Container>
           }
         />
