65bd74778c88f68a36f4fee50db90447647a0600
goldcrackle <harrisghanson@gmail.com>
2025-04-25 20:45:33 -0700

    Warversion1 (#4)
    * Enabled windows, fiddled with other windows things, and created the Watchwolfwar page.
    
    * Update Watchwolfwar.tsx
    
    * Set up the beginnings of server/sheets support, put a link on the header, fixed the filter.
    
    * Got the server to cooperate, got some numbers and such,  everything should be polished-ish!
    
    * Got the server to cooperate, got some numbers and such,  everything should be polished-ish!
    
    ---------
    
    Co-authored-by: bones-bones <elliot.ma.brown@gmail.com>

--- src/TeamWolf.tsx
+++ src/TeamWolf.tsx
@@ -1,6 +1,17 @@
-export const TeamWolf = async (value: string) => {
+export const TeamWolf = async () => {
   const requestedData = await fetch(
     "https://get-watch-wolf-war-821285593003.us-central1.run.app/"
+  );
+  const asJson = (await requestedData.json()) as {
+    data: { Name: string; Number: number }[];
+  };
+  return asJson.data;
+};
+
+export const TeamClock = async (value: string) => {
+  const requestedData = await fetch(
+    "https://get-watch-wolf-war-821285593003.us-central1.run.app/",
+    { method: "POST", body: JSON.stringify({ Name: value }) }
   );
   const asJson = (await requestedData.json()) as {
     data: { Name: string; Number: number }[];
