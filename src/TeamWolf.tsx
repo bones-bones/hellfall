d66b368182e1cfd2c0ce0633ab3fc9bb21adea35
bones-bones <elliot.ma.brown@gmail.com>
2025-04-25 17:10:52 -0700

    db,team wolf

--- src/TeamWolf.tsx
+++ src/TeamWolf.tsx
@@ -1,7 +1,9 @@
-export const TeamWolf = async (value:string) =>{
-    const requestedData = await fetch(
-        `https://sheets.googleapis.com/v4/spreadsheets/1gPM0QMZlToBBs6Clz5Brm3P68NG3xdhfCCm0DrVTJrs/values/WatchWolfWar Log?alt=json&key=${sheetsKey}`
-      );
-      const asJson = (await requestedData.json()) as any;  
-      return asJson;  
-}
+export const TeamWolf = async (value: string) => {
+  const requestedData = await fetch(
+    "https://get-watch-wolf-war-821285593003.us-central1.run.app/"
+  );
+  const asJson = (await requestedData.json()) as {
+    data: { Name: string; Number: number }[];
+  };
+  return asJson;
+};
