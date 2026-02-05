5e0d53fc6517414dbb37a9a08361d4cb94d3c99a
dracofulmen <max.popick@gmail.com>
2026-02-05 12:13:16 -0700

    Fixed Irresponsible
    This version correctly excludes creatures with the keyword "Irresponsible" from returning true

--- src/hellfall/canBeACommander.ts
+++ src/hellfall/canBeACommander.ts
@@ -2,9 +2,9 @@ import { HCEntry } from "../types";
 
 export const canBeACommander = (card: HCEntry) => {
   return (
-    (card["Supertype(s)"]?.[0]?.includes("Legendary") &&
+    ((card["Supertype(s)"]?.[0]?.includes("Legendary") &&
       card["Card Type(s)"][0]?.includes("Creature")) ||
-    (card["Text Box"]?.[0]?.includes("can be your commander") &&
-      !card["Text Box"]?.[0]?.includes("Irresponsible"))
+      card["Text Box"]?.[0]?.includes("can be your commander")) &&
+    !card["Text Box"]?.[0]?.includes("Irresponsible")
   );
 };
