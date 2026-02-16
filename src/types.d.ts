4ed8b1a7155913dddcf9c3781cdc99a6436c9b64
bones-bones <elliot.ma.brown@gmail.com>
2026-02-15 22:00:19 -0800

    fix types file

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,10 +11,17 @@ declare module '*.json' {
   export default value;
 }
 
-declare module "*.svg" {
-  const content: string;
-  export default content;
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  > & { title?: string }>;
+
+  const src: string;
+  export default src;
 }
+
 declare module "*.png" {
   const content: string;
   export default content;
