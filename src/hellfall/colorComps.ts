294be50c94c13fd442a8bfaa6dd91ed4ee7ad0d8
dracofulmen <max.popick@gmail.com>
2026-03-29 19:45:55 -0600

    colorless search fix

--- src/hellfall/colorComps.ts
+++ src/hellfall/colorComps.ts
@@ -17,47 +17,47 @@ const miscColors = [
 
 /**
  * Checks whether two color sets are the same colors.
- * @param hybridColors The first set of colors to compare
- * @param colors The second set of colors to compare
+ * @param colors1 The first set of colors to compare
+ * @param colors2 The second set of colors to compare
  * @returns boolean of whether the sets are the same colors.
  */
-export const sameColors = (hybridColors: HCColors | string[], colors: HCColors | string[]) => {
+export const sameColors = (colors1: HCColors | string[], colors2: HCColors | string[]) => {
   return (
-    hybridColors.length == colors.length &&
-    (hybridColors as string[]).every(color => (colors as string[]).includes(color))
+    colors1.length == colors2.length &&
+    (colors1 as string[]).every(color => (colors2 as string[]).includes(color))
   );
 };
 /**
  * Compares two sets of colors using an operator and returns a bool.
- * @param hybridColors The first set of colors to compare (must not include 'C' unless that is its only member; must not be empty)
+ * @param colors1 The first set of colors to compare (must not include 'C' unless that is its only member; must not be empty)
  * @param operator The operator
- * @param colors The second set of colors to compare (can include 'C' alongside other members, in which case 'C' is treated as the only member; must not be empty)
+ * @param colors2 The second set of colors to compare (can include 'C' alongside other members, in which case 'C' is treated as the only member; must not be empty)
  * @returns boolean of whether the comparison is true
  */
 export const colorCompOp = (
-  hybridColors: HCColors | string[],
+  colors1: HCColors | string[],
   operator: '<' | '<=' | '=' | '>=' | '>',
-  colors: HCColors | string[]
+  colors2: HCColors | string[]
 ) => {
-  if (colors.includes('C')) {
+  if (colors2.includes('C')) {
     switch (operator) {
       case '<': {
         return false;
       }
       case '<=': {
-        return hybridColors.includes('C');
+        return colors1.length==0;
       }
       case '=': {
-        return hybridColors.includes('C');
+        return colors1.length==0;
       }
       case '>=': {
         return true;
       }
       case '>': {
-        return !hybridColors.includes('C');
+        return colors1.length!=0;
       }
     }
-  } else if (hybridColors.includes('C')) {
+  } else if (colors1.length==0) {
     switch (operator) {
       case '<': {
         return true;
@@ -79,26 +79,26 @@ export const colorCompOp = (
     switch (operator) {
       case '<': {
         return (
-          hybridColors.length < colors.length &&
-          (hybridColors as string[]).every(color => (colors as string[]).includes(color))
+          colors1.length < colors2.length &&
+          (colors1 as string[]).every(color => (colors2 as string[]).includes(color))
         );
       }
       case '<=': {
-        return (hybridColors as string[]).every(color => (colors as string[]).includes(color));
+        return (colors1 as string[]).every(color => (colors2 as string[]).includes(color));
       }
       case '=': {
         return (
-          hybridColors.length == colors.length &&
-          (hybridColors as string[]).every(color => (colors as string[]).includes(color))
+          colors1.length == colors2.length &&
+          (colors1 as string[]).every(color => (colors2 as string[]).includes(color))
         );
       }
       case '>=': {
-        return (colors as string[]).every(color => (hybridColors as string[]).includes(color));
+        return (colors2 as string[]).every(color => (colors1 as string[]).includes(color));
       }
       case '>': {
         return (
-          hybridColors.length > colors.length &&
-          (colors as string[]).every(color => (hybridColors as string[]).includes(color))
+          colors1.length > colors2.length &&
+          (colors2 as string[]).every(color => (colors1 as string[]).includes(color))
         );
       }
     }
