a0d22abd3b9d8dbcfcbe1dfefcfbd3872043d0aa
dracofulmen <max.popick@gmail.com>
2026-04-08 21:37:01 -0600

    retry

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,10 +37,10 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/ban-ts-comment': 0, // i'm a sinner
     '@typescript-eslint/no-namespace': 0, // This is how scryfall does it and I don't want to rewrite their work more than I need to
-    'eqeqeq':'off',
-    'no-restricted-globals':'off',
-    'no-unused-vars':'off',
-    'react-hooks/exhaustive-deps':'off',
+    'eqeqeq':0,
+    'no-restricted-globals':0,
+    'no-unused-vars':0,
+    'react-hooks/exhaustive-deps':0,
 
     'react/self-closing-comp': [
       'error',
