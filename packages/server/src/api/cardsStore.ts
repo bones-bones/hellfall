cd697b71e9b9e21b159e80b67ad445d9c9b8e067
bones-bones <55368061+bones-bones@users.noreply.github.com>
2026-05-24 23:38:54 -0700

    Fix build (#69)
    * .

--- packages/server/src/api/cardsStore.ts
+++ packages/server/src/api/cardsStore.ts
@@ -1,16 +1,15 @@
-import { CollectionReference, Firestore } from '@google-cloud/firestore';
 import type { HCCard } from '@hellfall/shared/types';
-import { env } from './lib/env.ts';
-import cardsData from '@hellfall/shared/data/Hellscube-Database.json';
-
-const db = new Firestore({ databaseId: env.FIRESTORE_DATABASE_ID });
-const collection = db.collection(env.FIRESTORE_CARDS_COLLECTION);
+import { readDataJson } from '../lib/loadDataFiles.ts';
 
 const cardsMap: Map<string, HCCard.Any> = new Map(
-  (cardsData as { data: HCCard.Any[] }).data.map(card => [card.id.toLowerCase(), card])
+  readDataJson<{ data: HCCard.Any[] }>('Hellscube-Database.json').data.map(card => [
+    card.id.toLowerCase(),
+    card,
+  ])
 );
 
 export const getCardById = async (id: string): Promise<HCCard.Any | undefined> => {
-  return cardsMap?.get(id.toLowerCase());
+  return cardsMap.get(id.toLowerCase());
 };
+
 export const getAllCards = (): HCCard.Any[] => Array.from(cardsMap.values());
