68aa53fd43f139f6ef97b0128ff62cf6006ee446
dracofulmen <max.popick@gmail.com>
2026-08-04 19:00:07 -0600

    export fix

--- packages/shared/src/utils/cardHandling/index.ts
+++ packages/shared/src/utils/cardHandling/index.ts
@@ -2,7 +2,6 @@ export * from './mergeHCCardFaces.ts';
 export * from './orderProps.ts';
 export * from './cardMethods.ts';
 export * from './cardMap.ts';
-export * from './kindChecks.ts';
 export * from './propHandling.ts';
 export * from './printHandling.ts';
 export * from './invariantMap.ts';
