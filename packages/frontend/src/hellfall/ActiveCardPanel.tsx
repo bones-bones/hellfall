5d9aba87943a3ec0b8b6c92a46925481a4dcd975
bones-bones <elliot.ma.brown@gmail.com>
2026-08-09 12:39:17 -0700

    db update

--- packages/frontend/src/hellfall/ActiveCardPanel.tsx
+++ packages/frontend/src/hellfall/ActiveCardPanel.tsx
@@ -3,20 +3,11 @@ import { useKeyPress } from '../hooks';
 import { useAtom, useAtomValue } from 'jotai';
 import { activeCardAtom } from './atoms/searchAtoms';
 import { cardsAtom } from './atoms/cardsAtom';
-import {
-  Card,
-  ToolbarIconButton,
-  SidePanel,
-  useSidePanelModel,
-} from '@workday/canvas-kit-react';
+import { Card, ToolbarIconButton, SidePanel, useSidePanelModel } from '@workday/canvas-kit-react';
 import { externalLinkIcon, xIcon } from '@workday/canvas-system-icons-web';
 import { HellfallCard } from './card/HellfallCard';
 import { createStencil, createStyles } from '@workday/canvas-kit-styling';
-import {
-  createStenciledButtonDiv,
-  createStyledDiv,
-  StenciledButtonDivProps,
-} from '../styling';
+import { createStenciledButtonDiv, createStyledDiv, StenciledButtonDivProps } from '../styling';
 
 type dragCursor = 'w-resize' | 'ew-resize' | 'e-resize';
 interface ActiveCardPanelProps {
